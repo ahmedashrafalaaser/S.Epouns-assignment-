20512313b6266c4d6a1a1de20ed1a8cffe8a22d5
ahmedashrafalaaser <ahmedashrafalaaser@gmail.com>
2018-12-27 23:50:59 +0200

    first commit for book assignment

---
+++ function.js
@@ -0,0 +1,14 @@
+
+
+
+ function generateCover() {
+  
+    document.getElementById("title").innerHTML = document.getElementById("titleIn").value;
+    document.getElementById("subtitle").innerHTML = document.getElementById("subtitleIn").value;
+    document.getElementById("publisher").innerHTML = document.getElementById("publisherIn").value;
+    document.getElementById("date").innerHTML = document.getElementById("dateIn").value;
+    document.getElementById("right").style.backgroundColor = document.getElementById("bgIn").value;
+    
+  }
+
+
